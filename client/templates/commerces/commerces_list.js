1d7c0374e74a187b86141f5a5bf795e398692f5e
Menesys <camille.vasseur1995@gmail.com>
2016-02-26 02:35:58 +0100

    initial commit

---
+++ client/templates/commerces/commerces_list.js
@@ -0,0 +1,22 @@
+var commerceData = [
+  {
+    _id: '1',
+    nom: 'Boulangerie Bannette',
+    adresse: '53 rue Gabriel Péri',
+    image: 'commerce1.jpg'
+  },
+  {
+    _id: '2',
+    nom: 'Maison Perrin Boulanger-Patissier',
+    adresse: '62 cours Emile Zola',
+    image: 'commerce2.JPG'
+  },
+  {
+    _id: '3',
+    nom: 'Boulangerie Bannette',
+    adresse: '53 rue Gabriel Péri',
+    image: 'boulangerie.jpg'
+  }
+];
+Template.commerceList.helpers({
+});
